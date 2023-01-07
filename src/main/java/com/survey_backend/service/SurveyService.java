b03005a0df0e5885db5a1f9d6512153616b0a510
jaehyeon2 <wol5971@gmail.com>
2023-01-07 22:38:53 +0900

    feat: SurveyService (01.07)

---
+++ src/main/java/com/survey_backend/service/SurveyService.java
@@ -0,0 +1,24 @@
+package com.survey_backend.service;
+
+import com.survey_backend.domain.Survey;
+import com.survey_backend.repository.SurveyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class SurveyService {
+
+    private final SurveyRepository surveyRepository;
+
+    public Survey saveSurvey(Survey survey){
+        return surveyRepository.save(survey);
+    }
+
+    public Optional<Survey> findSurveyWithAnswerAndSection(Survey survey){
+        return surveyRepository.findSurveyWithSectionUsingJoin(survey.getId());
+    }
+}
