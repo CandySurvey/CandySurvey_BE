8b6f4ce5452c4fd6f1c1337220abf684cc58830e
jaehyeon2 <wol5971@gmail.com>
2023-02-01 20:53:52 +0900

    add: SurveyController (02.01)

--- src/main/java/com/survey_backend/Controller/SurveyController.java
+++ src/main/java/com/survey_backend/Controller/SurveyController.java
@@ -1,4 +1,35 @@
 package com.survey_backend.Controller;
 
+import com.survey_backend.repository.ItemRepository;
+import com.survey_backend.repository.QuestionRepository;
+import com.survey_backend.repository.SurveyRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RestController
 public class SurveyController {
+    private final SurveyRepository surveyRepository;
+    private final QuestionRepository questionRepository;
+    private final ItemRepository itemRepository;
+
+    @PostMapping("/create_survey")
+    public void saveSurvey(HttpServletRequest request) throws IOException {
+
+    }
+
+    @GetMapping("/survey/{code}")
+    public String loadSurvey(@RequestParam String code){
+        return "";
+    }
+
 }
+
