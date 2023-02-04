d0ffa1da77388604f21a1d583c425b79dc4682e9
jaehyeon2 <wol5971@gmail.com>
2023-02-04 20:32:24 +0900

    fix: Controller (Answer, Survey) (02.04)

--- src/main/java/com/survey_backend/Controller/SurveyController.java
+++ src/main/java/com/survey_backend/Controller/SurveyController.java
@@ -1,25 +1,23 @@
 package com.survey_backend.Controller;
 
-import com.survey_backend.repository.ItemRepository;
-import com.survey_backend.repository.QuestionRepository;
-import com.survey_backend.repository.SurveyRepository;
+import com.survey_backend.service.ItemService;
+import com.survey_backend.service.QuestionService;
+import com.survey_backend.service.SurveyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
 public class SurveyController {
-    private final SurveyRepository surveyRepository;
-    private final QuestionRepository questionRepository;
-    private final ItemRepository itemRepository;
+    private final SurveyService surveyService;
+    private final QuestionService questionService;
+    private final ItemService itemService;
 
     @PostMapping("/create_survey")
     public void saveSurvey(HttpServletRequest request) throws IOException {
