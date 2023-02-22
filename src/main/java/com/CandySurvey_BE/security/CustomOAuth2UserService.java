6dfc53877b8ec48704b952416277f19254427692
jaehyeon2 <wol5971@gmail.com>
2023-02-22 21:30:56 +0900

    feat: CustomOAuth2UserService (02.22)

---
+++ src/main/java/com/CandySurvey_BE/security/CustomOAuth2UserService.java
@@ -0,0 +1,29 @@
+package com.CandySurvey_BE.security;
+
+import com.CandySurvey_BE.repository.MemberRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final MemberRepository memberRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUSer(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException{
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuth
+    }
+}
